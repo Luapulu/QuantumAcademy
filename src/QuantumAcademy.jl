7bb4f245f244a9ac299d28b31cc634fc59ae2a51
Paul Nemec <paul.nemec@tum.de>
2024-04-23 16:15:52 +0100

    Changes from weekend

--- src/QuantumAcademy.jl
+++ src/QuantumAcademy.jl
@@ -1,3 +1,88 @@
 module QuantumAcademy
 
+import Base
+
+using Base: Callable
+using LinearAlgebra: eigen
+
+export FinDiffHamiltonian, DenseEigenProp, wavefunction
+
+struct FinDiffHamiltonian{T <: Real, F <: Callable} <: AbstractMatrix{T}
+    potential::F
+    length::T
+    width::T
+    nlength::Int
+    nwidth::Int
+end
+
+function FinDiffHamiltonian{T}(
+    potential::F,
+    length::T,
+    width::T,
+    nlength::Int,
+    nwidth::Int
+) where {T <: Real, F <: Callable}
+    FinDiffHamiltonian{T, F}(potential, length, width, nlength, nwidth)
+end
+
+function Base.size(H::FinDiffHamiltonian)
+    N = H.nlength * H.nwidth
+    return (N, N)
+end
+
+function _to_cartesian_index(i, n)
+    d = fld(i-1, n)
+    ix = i - d * n
+    iy = d + 1
+    return ix, iy
+end
+
+function Base.getindex(H::FinDiffHamiltonian{T}, i::Int, j::Int) where T
+    ix, iy = _to_cartesian_index(i, H.nlength)
+    jx, jy = _to_cartesian_index(j, H.nlength)
+
+    if ix == jx
+        if iy == jy
+            Vij = H.potential(T((ix-1) // (H.nlength - 1)), T((iy-1) // (H.nwidth - 1)))
+            return (T(H.nlength) / H.length)^2 + (T(H.nwidth) / H.width)^2 + Vij
+        elseif abs(iy - jy) == 1 || abs(iy - jy) == H.nlength - 1
+            return -(T(H.nwidth) / H.width)^2 / 2
+        end
+    elseif iy == jy && (abs(ix - jx) == 1 || abs(ix - jx) == H.nwidth - 1)
+        return -(T(H.nlength) / H.length)^2 / 2
+    end
+
+    return zero(T)
+end
+
+struct DenseEigenProp{T <: Real, TH}
+    hamiltonian::TH
+    values::Vector{T}
+    vectors::Matrix{Complex{T}}
+    scratch::Matrix{Complex{T}}
+end
+
+function DenseEigenProp{T}(hamiltonian::TH) where {T <: Real, TH}
+    vals, vecs = eigen(convert(Matrix{T}, hamiltonian); sortby=nothing)
+    DenseEigenProp{T, TH}(hamiltonian, vals, vecs, similar(vecs))
+end
+
+function DenseEigenProp(hamiltonian::TH) where TH
+    DenseEigenProp{eltype(TH)}(hamiltonian)
+end
+
+function (prop::DenseEigenProp)(t::Real)
+    for j in axes(prop.hamiltonian, 1)
+        c = cis(-prop.values[j] * t)
+        for i in axes(prop.hamiltonian, 1)
+            prop.scratch[i, j] = c * prop.vectors[i, j]
+        end
+    end
+    prop.scratch * prop.vectors'
+end
+
+wavefunction(ψ, nlength, nwidth) = vec([
+    ψ(x, y) for x in range(0, 1; length=nlength), y in range(0, 1; length=nwidth)
+])
+
 end # module QuantumAcademy
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,50 @@
 using Test, QuantumAcademy
+using Unitful, UnitfulAtomic
+using LinearAlgebra: UniformScaling
 
-@testset "QuantumAcademy" begin
-    @test true
+@testset "PeriodicFinDiffHamiltonian" begin
+    @testset "Constant Potential" begin
+        V(x, y) = 5.0
+        L = austrip(100u"nm")
+        W = austrip(200u"nm")
+
+        NL = 6
+        NW = 4
+
+        H = FinDiffHamiltonian{Float64}(V, L, W, NL, NW)
+
+        @test H.length isa Float64
+        @test H.width isa Float64
+        @test H.nlength isa Int
+        @test H.nwidth isa Int
+
+        @test eltype(H) == Float64
+        @test size(H) == (NL * NW, NL * NW)
+
+        ial2 = (NL / L)^2
+        iaw2 = (NW / W)^2
+        @test H[1:3, 1:3] ≈ [
+            5+ial2+iaw2      -ial2/2            0;
+            -ial2/2      5+ial2+iaw2      -ial2/2;
+                      0      -ial2/2  5+ial2+iaw2]
+
+        U = DenseEigenProp(H)
+
+        @test U(0) ≈ Matrix(UniformScaling(1), NL * NW, NL * NW)
+
+        nx = 5
+        ny = 7
+        ψ0 = wavefunction((x, y) -> cispi(2 * nx * x) * cispi(2 * ny * y), NL, NW)
+        t1 = austrip(100u"ns")
+
+        ψtest1 = cis( t1 * (
+            -5.0 +
+            -2 * nx^2 / L^2 * sinpi(2*nx/NL)^2 +
+            -2 * ny^2 / W^2 * sinpi(2*ny/NW)^2
+        )) .* ψ0
+
+        @show maxerr = maximum(abs2, U(t1) * ψ0 - ψtest1)
+        @show abs2.(U(t1) * ψ0 - ψtest1)
+        @test U(t1) * ψ0 ≈ ψtest1
+    end
 end
