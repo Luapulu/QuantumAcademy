a12967d609bff1fe8a0cb58c02db7dfe6745c48d
theofem2 <ge49wed@mytum.de>
2024-06-23 20:09:40 +0200

    Added function to calculate band structure; Not yet tested

--- src/QuantumAcademy.jl
+++ src/QuantumAcademy.jl
@@ -13,6 +13,8 @@ struct FinDiffHamiltonian{T <: Real, F <: Callable} <: AbstractMatrix{T}
     width::T
     nlength::Int
     nwidth::Int
+    kpointx::T
+    kpointy::T
 end
 
 function FinDiffHamiltonian{T}(
@@ -20,9 +22,11 @@ function FinDiffHamiltonian{T}(
     length::Real,
     width::Real,
     nlength::Int,
-    nwidth::Int
+    nwidth::Int;
+    kpointx::T=0.0,
+    kpointy::T=0.0
 ) where {T <: Real, F <: Callable}
-    FinDiffHamiltonian{T, F}(potential, length, width, nlength, nwidth)
+    FinDiffHamiltonian{T, F}(potential, length, width, nlength, nwidth, kpointx, kpointy)
 end
 
 function Base.size(H::FinDiffHamiltonian)
@@ -44,12 +48,12 @@ function Base.getindex(H::FinDiffHamiltonian{T}, i::Int, j::Int) where T
     if ix == jx
         if iy == jy
             Vij = H.potential(T((ix-1) // H.nlength), T((iy-1) // H.nwidth))
-            return (T(H.nlength) / H.length)^2 + (T(H.nwidth) / H.width)^2 + Vij
+            return (T(H.nlength) / H.length)^2 + (T(H.nwidth) / H.width)^2 + π^2/2*(1/H.length^2*H.kpointx^2 + 1/H.width^2*H.kpointy^2) + Vij
         elseif abs(iy - jy) == 1 || abs(iy - jy) == H.nwidth - 1
-            return -(T(H.nwidth) / H.width)^2 / 2
+            return -(T(H.nwidth) / H.width)^2 / 2 - H.kpointy*H.nwidth/H.width^2*π/2*sign(iy-jy - fld(iy,H.nwidth)*H.nwidth + fld(jy,H.nwidth)*H.nwidth)im
         end
     elseif iy == jy && (abs(ix - jx) == 1 || abs(ix - jx) == H.nlength - 1)
-        return -(T(H.nlength) / H.length)^2 / 2
+        return -(T(H.nlength) / H.length)^2 / 2 - H.kpointx*H.nlength/H.length^2*π/2*sign(ix-jx - fld(ix,H.nlength)*H.nlength + fld(jx,H.nlength)*H.nlength)im
     end
 
     return zero(T)
@@ -87,6 +91,16 @@ function wavefunction(ψ, nlength, nwidth; normalise=true)
     v = vec([ψ(x, y) for x in rx, y in ry])
     normalise && normalize!(v)
     return v
+end
+
+function calculate_bandstructure(V, L, W, NL, NW, path)
+    #This function serves to calculate the band structure of the cell. The basic assumption is that the periodic boundary conditions are representative of an infinite perfect crystal, so the Hamiltonian can be rewritten as one for Bloch functions. We'll use the existing (finite difference) numerical implementation of the Hamiltonian, and only add the option to specify the k-point. The Hamiltonian then must be diagonalised at each k-point (So, very expensive!), and this gives us the energy levels at that k-point. Then simply return a list of energy eigenvalues for every k-point in path. Note that the eigenvectors are not the true wavefunctions, but the Bloch-functions.
+    energies = Array{Float64}(undef, NL*NW, size(path,2))
+    for i in 1:size(path,2)
+        H = FinDiffHamiltonian{Float64}(V, L, W, NL, NW, path[1,i], path[2,i])
+        energies[:,i] = eigvals(convert(Hermitian{T}, hamiltonian))
+    end
+    return energies
 end
 
 end # module QuantumAcademy
